80cf67f44927ce0514c90836e6475e3fb50cdb1e
Umar M. Sheikh <umar.m.sheikh@gmail.com>
2014-01-31 22:31:18 +0500

    now works for both utf-8 and ISO8859-1

--- app/controllers/csv_files_controller.rb
+++ app/controllers/csv_files_controller.rb
@@ -14,12 +14,18 @@ class CsvFilesController < ApplicationController
     require 'csv'
     file = params[:csv_file][:file]
     @result = []
-    CSV.foreach(file.tempfile.path, encoding: "iso-8859-1") do |row|
-      @result << row
+    encodings = ['utf-8', "ISO8859-1"]
+    index = 0
+    begin
+      CSV.foreach(file.tempfile.path, encoding: encodings[index]) do |row|
+        @result << row
+      end
+    rescue Exception => ex
+      index += 1
+      @result = []
+      retry if index < encodings.length
+      @error = ex.message
     end
-  rescue => ex
-    @result = nil
-    @error = ex.message
   end
 
 end
